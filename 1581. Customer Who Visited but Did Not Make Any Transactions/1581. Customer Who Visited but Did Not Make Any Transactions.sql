51c2565d2c32a766d62ff911c74d3964e0d64725
allanleandrof <alanleandrof@gmail.com>
2025-01-31 12:07:25 -0300

    feat: add 1581. Customer Who Visited but Did Not Make Any Transactions made in mysql

---
+++ 1581. Customer Who Visited but Did Not Make Any Transactions/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,8 @@
+WITH SemTransacoes AS (
+    SELECT V.visit_id, V.customer_id
+    FROM Visits V
+    LEFT JOIN Transactions T ON V.visit_id = T.visit_id
+    WHERE T.transaction_id IS NULL
+)
+SELECT customer_id, COUNT(*) AS count_no_trans
+FROM SemTransacoes
